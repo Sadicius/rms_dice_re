ec720ff29c00f33282a40ec1a14a5dd6a95fc0fa
Sadicius <124639760+Sadicius@users.noreply.github.com>
2023-08-30 22:40:17 +0200

    Update rms_dices2.lua

--- rms_dices2.lua
+++ rms_dices2.lua
@@ -1,12 +1,11 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
+local diceNumber = math.random(1, 6)
 
 RSGCore.Functions.CreateUseableItem("dice", function(source, item)
     local Player = RSGCore.Functions.GetPlayer(source)
-    local diceNumber = math.random(1, 6)
-        if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+    --local diceNumber = math.random(1, 6)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
         -- Generar un número aleatorio de 1 a 6 para el dado
-       
-        --TriggerClientEvent('rms_dice:ToggleDiceAnim', source)
         TriggerClientEvent("rms_dice:client:OpenDice", source)
         Wait(2000)
         local mensajeChat = "¡El resultado del dado es: " .. diceNumber .. "!"
