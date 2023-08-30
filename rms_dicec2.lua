850b69fdcb4422226dd656807753c32db2e9f78e
Sadicius <124639760+Sadicius@users.noreply.github.com>
2023-08-30 22:41:01 +0200

    Update rms_dicec2.lua

--- rms_dicec2.lua
+++ rms_dicec2.lua
@@ -15,7 +15,9 @@ local function OpenDiceInterface()
     local ped = PlayerPedId()
     local ped_coords = GetEntityCoords(ped)
     SetNuiFocus(true, true)
-    SendNUIMessage({ show = true })
+    SendNUIMessage({ 
+        show = true;
+})
 end
 
 -- Callback para cerrar la interfaz del dado
@@ -30,20 +32,10 @@ RegisterNUICallback('escape', function(data, cb)
     cb('ok')
 end)
 
--- Evento para activar una animación relacionada con el dado
---[[ RegisterNetEvent("rms_dice:ToggleDiceAnim")
-AddEventHandler("rms_dice:ToggleDiceAnim", function()
-    TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('WORLD_PLAYER_PICKUP_WEAPON_THROWN_TOMAHAWK_ANCIENT'), -1, true, false, false, false)
-end)
-
--- Evento para abrir la interfaz del dado
 RegisterNetEvent('rms_dice:client:OpenDice')
 AddEventHandler('rms_dice:client:OpenDice', function()
-    OpenDiceInterface()
-end)  ]]
-
-RegisterNetEvent('rms_dice:client:OpenDice')
-AddEventHandler('rms_dice:client:OpenDice', function()
+    
+    -- Ahora la función OpenDice recibe el argumento diceNumber
     TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('WORLD_PLAYER_PICKUP_WEAPON_THROWN_TOMAHAWK_ANCIENT'), -1, true, false, false, false)
     OpenDiceInterface()
 end)
